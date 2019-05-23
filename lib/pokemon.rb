58fddcc4adec2a04f798e37a71ac934c0debc5f9
JackH17 <jack.hawkins77@googlemail.com>
2019-05-23 17:29:56 +0100

    all tests passing

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,44 @@
 class Pokemon
+
+  require 'pry'
+
+attr_accessor :name, :type, :id, :db
+
+def initialize(props={})
+   @id = props['id']
+   @name = props['name']
+   @type = props['type']
+   @db = db
+ end
+
+def self.save(name, type, db)
+
+  sql = <<-SQL
+  INSERT INTO pokemon(name, type)
+  VALUES (?, ?)
+  SQL
+  db.execute(sql, name, type)
+  @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+end
+
+def self.new_from_db(row)
+  new_pokemon = self.new
+  new_pokemon.id = row[0]
+  new_pokemon.name = row[1]
+  new_pokemon.type = row[2]
+  new_pokemon
+end
+
+def self.find(id, db)
+  sql = <<-SQL
+  SELECT *
+  FROM pokemon
+  WHERE id = ?
+  LIMIT 1
+  SQL
+  db.execute(sql, id).map{|row|
+  self.new_from_db(row)}[0]
+  end
+
+
 end
